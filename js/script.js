631d5699525194433b23648390a4f7d16eb8eb06
albertholskog <91052944+albertholskog@users.noreply.github.com>
2022-04-29 22:21:03 +0200

    list post create html

--- js/script.js
+++ js/script.js
@@ -1,53 +1,64 @@
-// const hamburger = document.querySelector(".hamburger");
-// const navMenu = document.querySelector(".header-nav-menu");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".header-nav-menu");
 
-// hamburger.addEventListener("click", () => {
-//     hamburger.classList.toggle("active");
-//     navMenu.classList.toggle("active");
-// });
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-// document.querySelectorAll(".nav-link").forEach((e) =>
-//     e.addEventListener("click", () => {
-//         hamburger.classList.remove("active");
-//         navMenu.classList.remove("active");
-//     })
-// );
-
-// const queryString = document.location.search;
-// const params = new URLSearchParams(queryString);
-
-// console.log(params);
-
-// const id = params.get("id");
-// console.log(id)
-//  const idName = id.slice(0, -1);
-//  console.log(idName);
-
+document.querySelectorAll(".nav-link").forEach((e) =>
+  e.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+// --------function for henting av apiCall----------------
 const url = "https://beardland.hols.no/wp-json/wp/v2/posts/";
-const containerSecondary = document.querySelector(".secondary-container");
 
 async function apiCall(url) {
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
-    } catch (e) {}
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (e) {}
 }
+// -------------------container for hovedsiden------------
+const containerPrim = document.querySelector(".prim-container");
+const containerSecondary = document.querySelector(".secondary-container");
 
-
-
-
+// ------------main page call api----------------
 async function htmlCreate() {
   const blogData = await apiCall(url);
-    containerSecondary.innerHTML += `
-  <div class="img-container"><img src="${blogData[0].featured_media_src_url}" alt=""></div>
-  <div>
-  <h2>${blogData[0].title.rendered}</h2>
-  <p>${blogData[0].acf.articles}</p>
-  <button class="btn-readmore">Read more</button>
-  </div>`;
+  containerPrim.innerHTML += ` <div>
+                                    <h2>${blogData[4].title.rendered}</h2>
+                                    <p>${blogData[4].acf.articles}</p>
+                                    <button class="btn-readmore">Read more</button>
+                                </div>
+                                <div class="img-container"><img src="${blogData[4].featured_media_src_url}" alt=""></div>`;
+  containerSecondary.innerHTML += `
+                                <div class="img-container"><img src="${blogData[5].featured_media_src_url}" alt=""></div>
+                                <div>
+                                    <h2>${blogData[5].title.rendered}</h2>
+                                    <p>${blogData[5].acf.articles}</p>
+                                    <button class="btn-readmore">Read more</button>
+                                </div>`;
 }
 htmlCreate();
+
+const listPost = document.querySelector(".list-container");
+
+async function createList() {
+  const blogData = await apiCall(url);
+  for (let i = 0; i < blogData.length; i++) {
+    console.log(blogData[i]);
+    listPost.innerHTML += `<div class="card">
+                                <h2>${blogData[i].title.rendered}</h>
+                                <img src="${blogData[i].featured_media_src_url}" alt="" />
+                                 <button class="btn-readmore">Read more</button>
+                            </div>`;
+  }
+}
+createList();
 
 // async function callApi() {
 
