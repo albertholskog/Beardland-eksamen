716068a499b1d2f8f924dd682a2e6b3045d81c61
albertholskog <91052944+albertholskog@users.noreply.github.com>
2022-05-01 14:42:55 +0200

    list api call done

--- js/script.js
+++ js/script.js
@@ -28,80 +28,40 @@ const containerSecondary = document.querySelector(".secondary-container");
 
 // ------------main page call api----------------
 async function htmlCreate() {
-  const blogData = await apiCall(url);
-  containerPrim.innerHTML += ` <div>
-                                    <h2>${blogData[4].title.rendered}</h2>
-                                    <p>${blogData[4].acf.articles}</p>
-                                    <button class="btn-readmore">Read more</button>
-                                </div>
-                                <div class="img-container"><img src="${blogData[4].featured_media_src_url}" alt=""></div>`;
-  containerSecondary.innerHTML += `
-                                <div class="img-container"><img src="${blogData[5].featured_media_src_url}" alt=""></div>
-                                <div>
-                                    <h2>${blogData[5].title.rendered}</h2>
-                                    <p>${blogData[5].acf.articles}</p>
-                                    <button class="btn-readmore">Read more</button>
-                                </div>`;
+  try {
+    const blogData = await apiCall(url);
+    containerPrim.innerHTML += ` <div>
+                                      <h2>${blogData[4].title.rendered}</h2>
+                                      <p>${blogData[4].acf.articles}</p>
+                                      <button class="btn-readmore">Read more</button>
+                                  </div>
+                                  <div class="img-container"><img src="${blogData[4].featured_media_src_url}" alt=""></div>`;
+    containerSecondary.innerHTML += `
+                                  <div class="img-container"><img src="${blogData[5].featured_media_src_url}" alt=""></div>
+                                  <div>
+                                      <h2>${blogData[5].title.rendered}</h2>
+                                      <p>${blogData[5].acf.articles}</p>
+                                      <button class="btn-readmore">Read more</button>
+                                  </div>`;
+  } catch (e) {}
 }
 htmlCreate();
 
-// async function createList() {
-//   const blogData = await apiCall(url);
-//   for (let i = 2; i < blogData.length; i++) {
-//     console.log(blogData[i]);
-//     listPost.innerHTML += `<div class="card">
-//                                 <h2>${blogData[i].title.rendered}</h>
-//                                 <img src="${blogData[i].featured_media_src_url}" alt="" />
-//                                  <button class="btn-readmore">Read more</button>
-//                             </div>
-//                             <div class="card">
-//                                 <h2>${blogData[i].title.rendered}</h>
-//                                 <img src="${blogData[i].featured_media_src_url}" alt="" />
-//                                  <button class="btn-readmore">Read more</button>
-//                             </div><div class="card">
-//                                 <h2>${blogData[i].title.rendered}</h>
-//                                 <img src="${blogData[i].featured_media_src_url}" alt="" />
-//                                 <button class="btn-readmore">Read more</button>
-//                            </div>`;
-//   }
-// }
-// createList();
-// const listPost = document.querySelector(".list-container");
+// ------------ list articles page-------------------
 
-// async function createList() {
-//   const apiData = await apiCall(url);
+const listPost = document.querySelector(".list-container");
 
-//   for (let i = 1; i < apiData.length; i++) {
-//     // const element = apiData[i];
-//     console.log(i);
+async function listArticles() {
+  try {
+    const blogData = await apiCall(url);
+    for (let i = 0; i < blogData.length; i++) {
+      listPost.innerHTML += ` <div class="card">
+                                        <h2>${blogData[i].title.rendered}</h>
+                                        <img src="${blogData[i].featured_media_src_url}" alt="" />
+                                        <button class="btn-readmore">Read more</button>
+                                    </div>`;
+    }
+  } catch (e) {}
+}
 
-//     for (let j = 2; j < apiData.length; j++) {
-//       // const element = apiData[j];
-//       console.log(j);
-//       console.log("hei");
-//     }
-//   }
-// }
-
-// createList();
-
-// async function callApi() {
-
-//     try {
-//         const blogResponse = await fetch(url);
-//         const blogdata = await blogResponse.json();
-//         console.log(blogdata[0]);
-
-//           console.log(blogdata[0].title.rendered)
-//           containerSecondary.innerHTML += `
-//           <div class="img-container"><img src="${blogdata[0].featured_media_src_url}" alt=""></div>
-//           <div>
-//           <h2>${blogdata[0].title.rendered}</h2>
-//           <p>${blogdata[0].acf.articles}</p>
-//           <button class="btn-readmore">Read more</button>
-//           </div>`;
-
-//     } catch (e) {}
-// }
-
-// callApi();
+listArticles();
