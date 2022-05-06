115cd1c5d7c234271faa6ad6f79b41450b519216
albertholskog <91052944+albertholskog@users.noreply.github.com>
2022-05-06 15:15:36 +0200

    contact form js done

--- js/contact.js
+++ js/contact.js
@@ -23,3 +23,58 @@ async function apiCall(url) {
     return data;
   } catch (e) {}
 }
+
+//--------------------form ------------------
+
+const form = document.querySelector("#contactForm");
+const fullName = document.querySelector("#fullName");
+const fullNameErr = document.querySelector("#fullname-err");
+
+const subject = document.querySelector("#subject");
+const subjectErr = document.querySelector("#subject-err");
+
+const messages = document.querySelector("#messages");
+const messagesErr = document.querySelector("#messages-err");
+
+const email = document.querySelector("#email");
+const emailErr = document.querySelector("#email-err");
+
+const validForm = (e) => {
+  e.preventDefault();
+
+  if (checkLength(fullName.value, 1)) {
+    fullNameErr.style.display = "none";
+  } else {
+    fullNameErr.style.display = "block";
+  }
+  if (emailValid(email.value)) {
+    emailErr.style.display = "none";
+  } else {
+    emailErr.style.display = "block";
+  }
+  if (checkLength(subject.value, 15)) {
+    subjectErr.style.display = "none";
+  } else {
+    subjectErr.style.display = "block";
+  }
+  if (checkLength(messages.value, 25)) {
+    messagesErr.style.display = "none";
+  } else {
+    messagesErr.style.display = "block";
+  }
+};
+
+form.addEventListener("submit", validForm);
+
+const checkLength = (value, len) => {
+  if (value.trim().length >= len) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const emailValid = (email) => {
+  const regEx = /\S+@\S+\.\S+/;
+  return regEx.test(email);
+};
