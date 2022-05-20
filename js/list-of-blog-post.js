ad52efdbd4064be13d78741a7138927f5726210f
albertholskog <91052944+albertholskog@users.noreply.github.com>
2022-05-20 22:40:39 +0200

    filer

--- js/list-of-blog-post.js
+++ js/list-of-blog-post.js
@@ -49,3 +49,59 @@ async function loadMore() {
     Error could not connect to api, try to refresh this page</p> `;
   }
 }
+
+const filterTipsUrl =
+  "https://beardland.hols.no/wp-json/wp/v2/posts/?categories=6";
+const filterGuideUrl =
+  "https://beardland.hols.no/wp-json/wp/v2/posts/?categories=4";
+const filterProductUrl =
+  "https://beardland.hols.no/wp-json/wp/v2/posts/?categories=5";
+
+const filterButtonTips = document.querySelector(".btn-filter-tips");
+const filterButtonGuide = document.querySelector(".btn-filter-guide");
+const filterButtonProduct = document.querySelector(".btn-filter-product");
+
+filterButtonTips.addEventListener("click", () => {
+  updateFilterTips();
+});
+filterButtonGuide.addEventListener("click", () => {
+  updateFilterTips();
+});
+filterButtonProduct.addEventListener("click", () => {
+  updateFilterTips();
+});
+
+async function updateFilterTips() {
+  try {
+    const blogData = await apiCall(filterTipsUrl);
+    test();
+    // listPost.innerHTML = "";
+    // for (let i = 0; i < blogData.length; i++) {
+    //   const element = blogData[i];
+    //   console.log(element);
+    //   listPost.innerHTML += ` <div class="card"><a href="/blog-post-specific.html?id=${blogData[i].id}">
+    //                             <h2>${blogData[i].title.rendered}</h2>
+    //                             <img src="${blogData[i].featured_media_src_url}" alt="${blogData[i].acf.alt}" />
+    //                             <button class="btn-readmore-list">Read more</button></a>
+    //                         </div>`;
+
+    //   loadMorebutton.style.display = "none";
+  } catch (e) {}
+}
+
+async function test() {
+  try {
+    listPost.innerHTML = "";
+    for (let i = 0; i < blogData.length; i++) {
+      const element = blogData[i];
+      console.log(element);
+      listPost.innerHTML += ` <div class="card"><a href="/blog-post-specific.html?id=${blogData[i].id}">
+                                <h2>${blogData[i].title.rendered}</h2>
+                                <img src="${blogData[i].featured_media_src_url}" alt="${blogData[i].acf.alt}" />
+                                <button class="btn-readmore-list">Read more</button></a>
+                            </div>`;
+
+      loadMorebutton.style.display = "none";
+    }
+  } catch (e) {}
+}
