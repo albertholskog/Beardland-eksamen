ab54fb0a4423a3ef3d27dc756a2b338bbe06345d
albertholskog <91052944+albertholskog@users.noreply.github.com>
2022-05-25 13:44:13 +0200

    list post er done

--- js/list-of-blog-post.js
+++ js/list-of-blog-post.js
@@ -35,26 +35,8 @@ const loadMoreUrl =
   "https://beardland.hols.no/wp-json/wp/v2/posts/?per_page=14";
 
 loadMorebutton.addEventListener("click", () => {
-  loadMore();
+  updateArticles(loadMoreUrl);
 });
-
-async function loadMore() {
-  try {
-    const blogData = await apiCall(loadMoreUrl);
-    for (let i = 10; i < blogData.length; i++) {
-      listPost.innerHTML += ` <div class="card"><a href="/blog-post-specific.html?id=${blogData[i].id}">
-                                    <h2>${blogData[i].title.rendered}</h2>
-                                    <img src="${blogData[i].featured_media_src_url}" alt="${blogData[i].acf.alt}" />
-                                    <button class="btn-readmore-list">Read more</button></a>
-                                </div>`;
-
-      loadMorebutton.style.display = "none";
-    }
-  } catch (e) {
-    listPost.innerHTML = `<div class="catch-err"><p >
-    Error could not connect to api, try to refresh this page</p></d> `;
-  }
-}
 
 const filterTipsUrl =
   "https://beardland.hols.no/wp-json/wp/v2/posts/?categories=6";
@@ -68,54 +50,18 @@ const filterButtonGuide = document.querySelector(".btn-filter-guide");
 const filterButtonProduct = document.querySelector(".btn-filter-product");
 
 filterButtonTips.addEventListener("click", () => {
-  updateFilterTips();
+  updateArticles(filterTipsUrl);
 });
 filterButtonGuide.addEventListener("click", () => {
-  updateFilterGuide();
+  updateArticles(filterGuideUrl);
 });
 filterButtonProduct.addEventListener("click", () => {
-  updateFilterProduct();
+  updateArticles(filterProductUrl);
 });
 
-async function updateFilterTips() {
+async function updateArticles(urlFilter) {
   try {
-    const blogData = await apiCall(filterTipsUrl);
-    listPost.innerHTML = "";
-    for (let i = 0; i < blogData.length; i++) {
-      listPost.innerHTML += ` <div class="card"><a href="/blog-post-specific.html?id=${blogData[i].id}">
-                                <h2>${blogData[i].title.rendered}</h2>
-                                <img src="${blogData[i].featured_media_src_url}" alt="${blogData[i].acf.alt}" />
-                                <button class="btn-readmore-list">Read more</button></a>
-                            </div>`;
-
-      loadMorebutton.style.display = "none";
-    }
-  } catch (err) {
-    listPost.innerHTML = `<div class="catch-err"><p >
-  Error could not connect to api, try to refresh this page</p></d> `;
-  }
-}
-async function updateFilterGuide() {
-  try {
-    const blogData = await apiCall(filterGuideUrl);
-    listPost.innerHTML = "";
-    for (let i = 0; i < blogData.length; i++) {
-      listPost.innerHTML += ` <div class="card"><a href="/blog-post-specific.html?id=${blogData[i].id}">
-                                <h2>${blogData[i].title.rendered}</h2>
-                                <img src="${blogData[i].featured_media_src_url}" alt="${blogData[i].acf.alt}" />
-                                <button class="btn-readmore-list">Read more</button></a>
-                            </div>`;
-
-      loadMorebutton.style.display = "none";
-    }
-  } catch (err) {
-    listPost.innerHTML = `<div class="catch-err"><p >
-    Error could not connect to api, try to refresh this page</p></d> `;
-  }
-}
-async function updateFilterProduct() {
-  try {
-    const blogData = await apiCall(filterProductUrl);
+    const blogData = await apiCall(urlFilter);
     listPost.innerHTML = "";
     for (let i = 0; i < blogData.length; i++) {
       listPost.innerHTML += ` <div class="card"><a href="/blog-post-specific.html?id=${blogData[i].id}">
