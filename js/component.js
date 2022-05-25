28387f1d5c53086eb029f30a860c598740a436aa
albertholskog <91052944+albertholskog@users.noreply.github.com>
2022-05-25 13:47:04 +0200

    globel api call

--- js/component.js
+++ js/component.js
@@ -1,11 +1,9 @@
 export const url = "https://beardland.hols.no/wp-json/wp/v2/posts/?per_page=10";
 
 export async function apiCall(url) {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  } catch (e) {}
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
 export const loaderContainer = document.querySelector(".loader");
